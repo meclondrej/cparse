2655327bd79b777e1214f3d962f69374051930a8
Ondrej Mecl <meclondrej@protonmail.com>
2023-02-25 18:21:00 +0100

    Added cparse.h
    Added the cparse.h file and the cparse_info file in it.

---
+++ cparse.h
@@ -0,0 +1,18 @@
+/*
+The CParse library by
+  ___            _           _   __  __           _
+ / _ \ _ __   __| |_ __ ___ (_) |  \/  | ___  ___| |
+| | | | '_ \ / _` | '__/ _ \| | | |\/| |/ _ \/ __| |
+| |_| | | | | (_| | | |  __/| | | |  | |  __/ (__| |
+ \___/|_| |_|\__,_|_|  \___|/ | |_|  |_|\___|\___|_|
+                          |__/
+*/
+
+// includes
+
+
+
+// functions
+char* cparse_info() {
+    return "The CParse library by Ondrej Mecl is available";
+}
