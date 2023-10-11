dd82ac5802d02e715d6ee8239bfce860e147411b
Ondrej Mecl <meclondrej@protonmail.com>
2023-10-11 19:42:22 +0200

    Added strtoint

---
+++ convs/strtoint.h
@@ -0,0 +1,33 @@
+#ifndef STRTOINT_H
+#define STRTOINT_H
+
+#include "chartobyte.h"
+
+/**
+ * Converts a char[] to int
+ * @param x The string to be converted
+ * @param len Length of str
+ * @param pRes Pointer to the result variable
+ * @returns Whether or not was the conversion successful
+ */
+bool strtoint(char x[], int len, int* pRes)
+{
+    if (len == 0)
+        return false;
+    int res = 0;
+    for (int i = 0; i < len; i++)
+    {
+        if (x[i] == '\0')
+            break;
+        unsigned char num;
+        if (!chartobyte(x[i], &num))
+            return false;
+        if (i != 0)
+            res *= 10;
+        res += (int)num;
+    }
+    *pRes = res;
+    return true;
+}
+
+#endif
